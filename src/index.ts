8022c0ae715fc907c4e3a6e0d0a23206f60fa6a3
yonatan.bendahan@gmail.com <yonatan.bendahan@gmail.com>
2023-06-30 08:32:19 +0300

    with codec

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-import { FFProbe } from "./ffprobe";
+import { FFProbe, FFProbeStream } from "./ffprobe";
 
 export function getCodecString(ffprobe: FFProbe): string {
-  return "";
+  const videoStream = ffprobe.streams.find((s) => s.codec_type === "video");
+  if (!videoStream) {
+    throw new Error("No video stream found");
+  }
+
+  const codec = videoStream.codec_tag_string;
+
+  if (!codec) {
+    throw new Error("No codec_tag_string found");
+  }
+
+  return codec;
 }
